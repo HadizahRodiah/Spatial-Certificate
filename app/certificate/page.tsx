593da1c4b98dff459fa36d5beeb03832bbcd9118
HadizahRodiah <123167408+HadizahRodiah@users.noreply.github.com>
2025-05-17 22:34:04 +0100

    Add files via upload

--- app/certificate/page.tsx
+++ app/certificate/page.tsx
@@ -1,6 +1,8 @@
-// page.js (Server Component)
+'use client';
+
+import React from 'react';
 import CertificateDisplay from './ClientCertificate';
 
-export default function Page() {
+export default function Page(): JSX.Element {
   return <CertificateDisplay />;
 }
